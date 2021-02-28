95f0244062ef88169339e6d3e67320f8e7bad551
bal3000 <bal3000286@googlemail.com>
2021-02-28 21:14:26 +0000

    markdown editor

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,13 @@
+import MDEditor from '@uiw/react-md-editor';
+
+import './text-editor.css';
+
+function TextEditor() {
+  return (
+    <div>
+      <MDEditor />
+    </div>
+  );
+}
+
+export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 function App(): JSX.Element {
   return (
     <div>
+      <TextEditor />
       <CodeCell refreshRate={1000} />
     </div>
   );
