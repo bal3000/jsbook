40cd5a5074d9c28b4e64cdd18fe4dd896871bbbb
bal3000 <bal3000286@googlemail.com>
2021-03-01 09:08:27 +0000

    styling markdown editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,45 @@
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 import './text-editor.css';
 
 function TextEditor() {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# header');
+  const editorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className='text-editor' ref={editorRef}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div className='text-editor card' onClick={() => setEditing(true)}>
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 }
