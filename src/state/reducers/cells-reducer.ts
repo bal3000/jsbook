1d08fb817dc5504b5e2ad51f5a8694f581b489dd
bal3000 <bal3000286@googlemail.com>
2021-03-04 09:03:59 +0000

    last of the reducer functions

--- src/state/reducers/cells-reducer.ts
+++ src/state/reducers/cells-reducer.ts
@@ -22,18 +22,46 @@ const reducer = produce((state: CellsState = initialState, action: Actions) => {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      return;
+      return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      return;
+      return state;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((o) => o === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
       return state;
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        id: randomId(),
+        type: action.payload.type,
+        content: '',
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex((o) => o === action.payload.id);
+      if (foundIndex === -1) {
+        state.order.push(cell.id);
+      }
+      state.order.splice(foundIndex, 0, cell.id);
+
       return state;
     default:
       return state;
   }
 });
+
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
+};
 
 export default reducer;
