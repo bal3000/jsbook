530b6929e984af7737b85edcf1903f85167e445c
bal3000 <bal3000286@googlemail.com>
2021-03-05 17:27:57 +0000

    add cell styles

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks';
+import AddCell from './add-cell/add-cell';
 import CellListItem from './cell-list-item/cell-list-item';
 
 function CellList() {
@@ -7,10 +9,18 @@ function CellList() {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 }
 
 export default CellList;
